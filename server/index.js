c99cc853284d6e9f50a9fbac8a63f524dfbb4416
Peter Christensen <peter.f.chris@gmail.com>
2019-07-01 10:07:22 -0600

    feat: package.json, initial file setup, imgs added

---
+++ server/index.js
@@ -0,0 +1,36 @@
+// Requirements
+
+require('dotenv').config()
+const express = require('express'),
+        session = require('express-session'),
+        massive = require('massive'),
+        authCtrl = require('./controllers/auth_controller'),
+        socketCtrl = require('./controllers/socket_controller')
+
+const app = express()
+const {CONNECTION_STRING, SERVER_PORT, SESSION_SECRET} = process.env
+
+// Middleware
+
+app.use(express.json())
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        saveUninitialized: false,
+        resave: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60
+        }
+    })
+)
+
+// Database Connection
+
+massive(CONNECTION_STRING).then((database) => {
+    app.set('db', database)
+    console.log('database set!', database.listTables())
+    app.listen(SERVER_PORT, () => console.log(`Port ${SERVER_PORT} is not screwed up`))
+})
+
+// Endpoints
+
