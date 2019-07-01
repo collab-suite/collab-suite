f560df5f8865785407d495b236e15f23f6e4550a
Ian Kaliszewski <ikalisz@yahoo.com>
2019-07-01 10:42:37 -0600

    feat: initial routing & more packages

---
+++ src/Components/DrawingRoom/DrawingRoom.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect} from 'react'
+
+function DrawingRoom() {
+
+}
+
+export default DrawingRoom
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeStyles from './HomeStyles'
+
+function Home() {
+    return (
+        <h1>Home</h1>
+    )
+}
+
+export default Home
