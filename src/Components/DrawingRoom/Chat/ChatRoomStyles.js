6a067c138336b961a018222c9798f0bd4d5eab61
Ian Kaliszewski <ikalisz@yahoo.com>
2019-07-01 16:23:59 -0600

    feat: redux and sockets setup

---
+++ src/Components/DrawingRoom/Chat/ChatRoomStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const ChatContainer = styled.div``
+
+export const ChatRoom = styled.ul`
+    list-style: none;
+`
+
+export const Message = styled.li``
+
+export const MessageForm = styled.form``
+
+export const ChatInput = styled.input``
+
+export const SendMessage = styled.button``
--- src/Components/DrawingRoom/DrawingRoom.js
+++ src/Components/DrawingRoom/DrawingRoom.js
@@ -1,7 +1,11 @@
 import React, {useState, useEffect} from 'react'
+import Chat from './Chat/Chat'
 
-function DrawingRoom() {
 
+function DrawingRoom(props) {
+    return (
+        <Chat socket={props.socket} />
+    )
 }
 
 export default DrawingRoom
