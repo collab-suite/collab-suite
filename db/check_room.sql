3500725a79432a11a851b9525b3b62e37c21b3c4
Ian Kaliszewski <ikalisz@yahoo.com>
2019-07-03 13:59:47 -0600

    feat: added room ctrl and fixed sockets

---
+++ db/check_room.sql
@@ -0,0 +1,1 @@
+select * from active_rooms where room_id = ${room_id}
--- server/controllers/socket_controller.js
+++ server/controllers/socket_controller.js
@@ -7,11 +7,13 @@ const SocketConnection = (server, app) => {
         console.log('a user has connected')
         socket.on('join room', async (userInfo) => {
             socket.join(userInfo.roomID)
+            const users = await db.get_au({room_id: userInfo.roomID})
             const data = await db.select_messages({roomID: userInfo.roomID})
-            io.to(userInfo.roomID).emit('joined room', data, userInfo)
+            io.to(userInfo.roomID).emit('joined room', data, users)
         })
-        socket.on('leave room', (roomID) => {
-            socket.leave(roomID)
+        socket.on('leave room', async (userInfo) => {
+            socket.leave(userInfo.roomID)
+            await db.remove({user_id: userInfo.id})
             socket.disconnect()
         })
         socket.on('message send', async (userInfo) => {
@@ -19,8 +21,9 @@ const SocketConnection = (server, app) => {
             const data = await db.select_messages({roomID: userInfo.roomID})
             io.to(userInfo.roomID).emit('message recieved', data)
         })
-        socket.on('end room', (userInfo) => {
+        socket.on('end room', async (userInfo) => {
             socket.leave(userInfo.roomID)
+            await db.delete_room('userInfo')
             io.to(userInfo.roomID).emit('end room')
         })
         socket.on('disconnect', () => {
